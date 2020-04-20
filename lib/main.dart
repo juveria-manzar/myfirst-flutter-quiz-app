69c76989a8bc696026f57db7df659e5ba9264c83
Juveria Manzar <juveriadot@gmail.com>
2020-04-20 06:21:21 +0530

    layout-widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() => runApp(MyApp());
 
-}
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home:Text('Hello!'));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var question=[
+      'What\'s your favourite Color? ',
+      'What\'s your favourite animal',
+    ]
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child:Text('Answer 1'),
+              onPressed:null,
+            ),
+            RaisedButton(
+              child:Text('Answer 2'),
+              onPressed:null,
+            ),
+            RaisedButton(
+              child:Text('Answer 3')
+              ,onPressed:null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
